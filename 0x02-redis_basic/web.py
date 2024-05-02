7496332ad5e062f4470276d7852135c221186d6f
amch-med23 <skyied.med2023@gmail.com>
2024-05-02 16:13:28 +0100

    adding the web.py script, that implements the get_page function

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""In this tasks, we will implement a get_page function
+(prototype: def get_page(url: str) -> str:)."""
+
+
+import redis
+import requests
+from functools import wraps
+
+redis = redis.Redis()
+
+
+def url_access_count(method):
+    """decorator for get_page function"""
+    @wraps(method)
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = redis.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+        # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        redis.incr(key_count)
+        redis.set(key, html_content, ex=10)
+        redis.expire(key, 10)
+        return html_content
+    return wrapper
+
+
+@url_access_count
+def get_page(url: str) -> str:
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
+    
